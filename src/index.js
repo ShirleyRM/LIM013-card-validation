826e859879ce04173435ab2776c1246e90651dce
Shirley Ramos_M <srm.shirley@gmail.com>
2020-08-07 11:11:12 -0500

    Prueba

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import validator from './validator.js';
 
 console.log(validator);
+  
+let btnValidar = document.getElementById("btnValidar");
+    btnValidar.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        let numeroTarjeta = document.getElementById("numeroTarjeta").value;
+      
+        let verificar = validator.isValid(numeroTarjeta);
+        let ocultar = validator.maskify(numeroTarjeta);
+           
+        
+        let divG=document.getElementById("validar");
+        if  (verificar === true) {
+            const show = "La tarjeta es VÁLIDA";
+
+            divG.innerHTML= show;
+        } else{
+              divG.innerHTML="La tarjeta es INVÁLIDA";
+        }
+         
+
+        let divS=document.getElementById("ocultar");
+        divS.innerHTML=ocultar;
+        
+
+    });   
+
+    // function suma(parametro){
+    //   return valorDeRetorno
+    // }
+
+    // suma(argumento)
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
-  // ...
-};
+  isValid:
+    function (numeroTarjeta) {
+      let creditCardNumber = numeroTarjeta.split("");
+      console.log(creditCardNumber);
 
-export default validator;
+      let numeroReverse = creditCardNumber.reverse();
+      console.log(numeroReverse);
+
+      let suma = 0;
+      for (let i = 0; i < numeroReverse.length; i++) {
+        let numeroPar = parseInt(numeroReverse[i]);
+        
+        if ((i % 2) != 0) {
+            numeroPar *= 2;
+          if (numeroPar > 9) {
+              numeroPar -= 9;
+          }
+          
+       
+        }
+        suma += numeroPar;
+     
+      }
+      console.log(suma);
+
+        if (suma % 10 == 0)
+
+        {    
+        return true;
+        }
+      else{
+        return false;
+       }
+      
+      },
+
+  maskify:(numeroTarjeta)=>{
+        let card = "";
+          for (let i = 0; i < numeroTarjeta.length; i++) {
+            if (i <= numeroTarjeta.length - 5) {
+              card = card + "#";
+              
+            } else {
+              card = card + numeroTarjeta [i]
+            }
+          }
+          console.log(card);
+          return card  
+      
+      }
+    };  
+      export default validator;
